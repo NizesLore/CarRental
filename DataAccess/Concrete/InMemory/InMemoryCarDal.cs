d3f551d90ec8f8bd92088bd22ff1896b392bd1e2
sezin sezin <fenciszn@hotmail.com>
2023-07-30 13:32:04 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+            new Car { CarId=1, BrandId=1, ColorId=1, ModelYear= 2023, DailyPrice=8000, Description="MERCEDES"},
+            new Car { CarId=2, BrandId=1, ColorId=2, ModelYear= 2022, DailyPrice=10000, Description="BMW"},
+            new Car { CarId=3, BrandId=2, ColorId=1, ModelYear= 2021, DailyPrice=12000, Description="VOLKSWAGEN"},
+            new Car { CarId=4, BrandId=2, ColorId=2, ModelYear= 2020, DailyPrice=14000, Description="AUDI"},
+            new Car { CarId=5, BrandId=3, ColorId=3, ModelYear= 2019, DailyPrice=16000, Description="TOYOTA"}
+
+        };
+                    
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId==car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetByBrand (int brandId)
+        {
+            return _cars.SingleOrDefault(c=> c.BrandId == brandId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+        //public List<Car> GetAllByBrand(int brandId)
+        //{
+        //    return _cars.Where(c=> c.BrandId== brandId).ToList();
+        //}
+    }
+}
